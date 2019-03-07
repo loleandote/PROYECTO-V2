1a79dd8bb93628aa4c57d861e7ef1fef51249d28
hola <hola>
2019-03-07 22:06:34 +0100

    commit 4

--- AlfonsoGit/src/Vehiculo.java
+++ AlfonsoGit/src/Vehiculo.java
@@ -1,6 +1,11 @@
+
 public class Vehiculo {
 	public Vehiculo(int numPuertas, int numRuedas) {
 		this.numPuertas = numPuertas;
 		this.numRuedas = numRuedas;
+	}
+
+	public void showinfo() {
+		System.out.println("Soy un vehiculo");
 	}
 }
